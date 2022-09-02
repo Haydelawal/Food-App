5cd2891ee5cd9b4550f6a06eb907d966cf079352
Lawal Hayde <81729609+Haydelawal@users.noreply.github.com>
2022-09-03 00:01:39 +0100

    Delete Paging3_NewsApp directory

--- Paging3_NewsApp/app/src/main/java/com/example/paging3_newsapp/model/NewsResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.example.paging3_newsapp.model
-
-import com.google.gson.annotations.SerializedName
-
-data class NewsResponse(
-    val articles: List<Article>,
-    val status: String,
-    val totalResults: Int
-)
